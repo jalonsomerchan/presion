8ba74cedf7fd4e524ed2996f6643d108f844c182
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-21 21:46:41 +0200

    Load grouped level packs

---
+++ src/data/levels.js
@@ -0,0 +1,6 @@
+import nivel1 from './nivel-1.json';
+import nivel2 from './nivel-2.json';
+import nivel3 from './nivel-3.json';
+import nivel4 from './nivel-4.json';
+
+export const levelPacks = [nivel1, nivel2, nivel3, nivel4];
