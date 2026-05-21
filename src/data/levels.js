58431042376663b074e524746668c28b8b0a85c0
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-21 22:40:14 +0200

    Load level JSON files automatically

--- src/data/levels.js
+++ src/data/levels.js
@@ -1,6 +1,5 @@
-import nivel1 from './nivel-1.json';
-import nivel2 from './nivel-2.json';
-import nivel3 from './nivel-3.json';
-import nivel4 from './nivel-4.json';
+const modules = import.meta.glob('./nivel-*.json', { eager: true });
 
-export const levelPacks = [nivel1, nivel2, nivel3, nivel4];
+export const levelPacks = Object.values(modules)
+  .map((module) => module.default)
+  .sort((firstLevel, secondLevel) => firstLevel.nivel - secondLevel.nivel);
